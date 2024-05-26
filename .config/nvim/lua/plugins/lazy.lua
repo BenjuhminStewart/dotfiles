8e59af1ab08fd91b3e46c576b053cba0cc456cd8
Benjamin Stewart <benjuhminstewart@gmail.com>
2024-05-25 23:05:13 -0700

    make autotags work on tsx

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -226,6 +226,17 @@ return {
 	},
 	{
 		"windwp/nvim-ts-autotag",
+		config = function()
+			require('nvim-ts-autotag').setup({})
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require('nvim-autopairs').setup({
+				disable_filetype = { "TelescopePrompt", "vim" },
+			})
+		end,
 	},
 	{
 		"EdenEast/nightfox.nvim",
